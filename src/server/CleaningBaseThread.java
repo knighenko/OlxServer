1e69150c919d95ea2acf0829f09232f02ca0ff5a
Maksym Knyzhenko <knighenko@gmail.com>
2021-04-07 21:20:58 +0300

    Implemented cunstructor CleaningBaseThread.java

--- src/server/CleaningBaseThread.java
+++ src/server/CleaningBaseThread.java
@@ -1,6 +1,11 @@
 package server;
 
 public class CleaningBaseThread extends Thread{
+    private String url;
+    public CleaningBaseThread( String url) {
+        this.url = url;
+
+    }
     @Override
     public void run() {
 
